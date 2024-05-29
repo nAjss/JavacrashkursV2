44d8f63b0acb7f97227ff9dd106e9bb440ebf1de
nAjss <42645550+nAjss@users.noreply.github.com>
2024-05-29 13:17:14 +0200

    Update d_verschachtelteSchleifen.java /2do/jj

--- JavacrashkursV2/src/g_Schleifen/d_verschachtelteSchleifen.java
+++ JavacrashkursV2/src/g_Schleifen/d_verschachtelteSchleifen.java
@@ -18,7 +18,8 @@ public class d_verschachtelteSchleifen {
 /* Wir können alle Schleifen ineinander verschachteln und so miteinander verknüpfen
 *MERKE: die festgelegten Variablen sind nur innerhalb des Codeblocks (innerhalb von {}) gültig
 wenn wir also ausserhalb der inneren for-Schleife versuchen den Wert von j auszugeben, wird eine fehlermeldung erscheinen,
-da die Variable nur innerhalb der inneren for_Schleife definiert ist!
+da die Variable nur innerhalb der inneren for_Schleife definiert ist! 
+Um dieses zu umgehen, kann man die Schleifensteuerungsvariablen außerhalb/über den for-Schleifen deklarieren, falls man die Zahlen z.B. für eine Aufzählung benötigt.
 -------------------------------------------------------------------
  * Im oben geschriebenen Code ist der Ablauf wie folgt:
  * 
@@ -26,7 +27,12 @@ da die Variable nur innerhalb der inneren for_Schleife definiert ist!
  * in der Inneren for-Schleife ist die anweisung
  * Wenn j kleiner als 4 ist (beginnend mit 1) dann gib im Teminal "Hey, listen " aus, dann addiere 1 zu j
  * die innere for-Schleife wird nun 3 mal ausgeführt (bis die bedingung j < 4 nicht mehr erfüllt ist) und gibt somit 3 Mal den String "Hey, listen " aus
- * da nun die innere for-Schleife abgearbeitet ist, springt der COde wieder zur äußeren for-Schleife und da i immernoch < 4 ist (gerade ist i = 2) 
- * wird der oben genannte vorgang wiederholt, bis die bedingung i < 4 nicht mehr erfüllt ist, also insgesamt 3 mal 
- * dadurch ergibt sich 3 man die ausgabe von 3mal "Hey, listen"
+ * da nun die innere for-Schleife abgearbeitet ist, springt der Code wieder zur äußeren for-Schleife und da i immernoch < 4 ist (gerade ist i = 2) 
+ * wird der oben genannte Vorgang wiederholt, bis die Bedingung i < 4 nicht mehr erfüllt ist, also insgesamt 3 mal 
+ * dadurch ergibt sich 3 man die Ausgabe von 3 mal "Hey, listen"
+
+ * edit: Die äußere Schleife läuft 3 mal. Für jeden dieser 3 Schritte läuft die innere Schleife 1 mal. Die innere Schleife läuft 3 mal und gibt bei jedem Schritt 1 mal "Hey listen" aus.
+ * insgesamt wird also 3 * 3 "Hey listen" ausgegeben. (äußere Schleife = 3 Durchläufe) * (innere Schleife = 3 Durchläufe)
+
+ 
  */
